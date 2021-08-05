26f4c61cffa98d8bb2b6df440b42b6fa045947b2
Olga0103 <nezabudka-84@mail.ru>
2021-08-05 21:07:00 +0300

    dz_3

---
+++ core/src/ru/gb/stargame/sprite/Background.java
@@ -0,0 +1,22 @@
+package ru.gb.stargame.sprite;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+
+import ru.gb.stargame.base.Sprite;
+import ru.gb.stargame.math.Rect;
+
+
+public class Background extends Sprite {
+
+    public Background(Texture texture) {
+        super(new TextureRegion(texture));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(worldBounds.getHeight());
+        pos.set(worldBounds.pos);
+    }
+}
